0e77a1e1e3abd532da5bdb02a31a4e97737780e9
leandro <leandro-simoes@hotmail.com>
2026-07-27 09:24:55 -0300

    fix(stf): remover --window-position conflitante com --headless=new no Chrome 150

--- scrapers.py
+++ scrapers.py
@@ -183,7 +183,6 @@ def _criar_driver_stf():
     options.add_argument('--headless=new')
     options.add_argument('--disable-gpu')
     options.add_argument('--window-size=1920,1080')
-    options.add_argument('--window-position=-32000,-32000')
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
     return uc.Chrome(options=options, version_main=VERSAO_CHROME_VM)
